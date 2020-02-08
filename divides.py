f62e642441bd8656fa4adac056bef87e4737b0dc
John Paul Lee <johnpaullee@Johns-MacBook-Pro.local>
2020-02-08 18:22:31 +0000

    Added Divides and Primes

---
+++ divides.py
@@ -0,0 +1,11 @@
+#John Paul Lee
+#Check if one number divides another
+
+p = 8
+m = 2
+
+if (p % m) ==0:
+    print(p, "divided by", m, "leaves a remainder of zero.")
+else:
+    print(p, "divided by", m, "does not leave a remainder of zero.")
+
